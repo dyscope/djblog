41ff468dfb688f4d41a4bba371730f2d7670de0c
dyscope <dyscope@gmail.com>
2024-07-27 17:44:55 +0300

    Initialization

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField("Title", max_length=100)
+    author = models.CharField("Author", max_length=100)
+    description = models.TextField("Post content")
+    date = models.DateField("Publication date")
+
+    def __str__(self) -> str:
+        return f"{self.title}, {self.author}"
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    
